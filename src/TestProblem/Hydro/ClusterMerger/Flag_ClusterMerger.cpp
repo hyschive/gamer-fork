8e8b34ed00be761d06496ca8b8e3d374cf95ddd5
Shuang-Shuang Chen <shuangshuang0411@gmail.com>
2022-10-11 15:28:46 +0800

    Modify the user-defined refinement criterion

--- src/TestProblem/Hydro/ClusterMerger/Flag_ClusterMerger.cpp
+++ src/TestProblem/Hydro/ClusterMerger/Flag_ClusterMerger.cpp
@@ -4,26 +4,8 @@
 
 
 extern int    Merger_Coll_NumHalos;
-
-extern double Jet_HalfHeight1;
-extern double Jet_HalfHeight2;
-extern double Jet_HalfHeight3;
-extern double Jet_Radius1;
-extern double Jet_Radius2;
-extern double Jet_Radius3;
- 
-extern double Jet_Vec[3][3]; // jet direction  
-
-/*
-double ClusterCen_Flag[3][3] = {  { NULL_REAL, NULL_REAL, NULL_REAL }, // cluster center       
-                                  { NULL_REAL, NULL_REAL, NULL_REAL },
-                                  { NULL_REAL, NULL_REAL, NULL_REAL }  };
-double BH_Vel_Flag[3][3] = {  { NULL_REAL, NULL_REAL, NULL_REAL }, // BH velocity
-                              { NULL_REAL, NULL_REAL, NULL_REAL },
-                              { NULL_REAL, NULL_REAL, NULL_REAL }  };  
-extern void GetClusterCenter( double Cen[][3], double BH_Vel[][3] );
-*/
-
+extern double R_acc;   // the radius to compute the accretoin rate 
+extern double Jet_Vec[3][3];   // jet direction  
 extern double ClusterCen[3][3];
 
 
@@ -53,46 +35,16 @@ bool Flag_ClusterMerger( const int i, const int j, const int k, const int lv, co
 
    bool Flag = false;
 
-//   GetClusterCenter( ClusterCen_Flag, BH_Vel_Flag );
-   double Jet_HalfHeight[3] = { Jet_HalfHeight1, Jet_HalfHeight2, Jet_HalfHeight3 };
-   double Jet_Radius[3] = { Jet_Radius1, Jet_Radius2, Jet_Radius3 }; 
-
-// Temp!!!
-   for (int d=0; d<3; d++)   ClusterCen[0][d] = 5.0;
+// Temporarily fix the jet direction vector!!!
    Jet_Vec[0][0] = 1.0;
    for (int d=1; d<3; d++)   Jet_Vec[0][d] = 0.0;
 
-// flag cells within the target radius
-   double Jet_dr, Jet_dh, S, Area;
-   double Dis_c2m, Dis_c2v, Dis_v2m, Vec_c2m[3], Vec_v2m[3];
-   double TempVec[3]; 
-
+// Flag cells within the target radius, and if the radius is not resolved with a specific number (Threshold[0]) of cells
    for (int c=0; c<Merger_Coll_NumHalos; c++)
    {
-//    distance: jet center to mesh
-      for (int d=0; d<3; d++)    Vec_c2m[d] = Pos[d] - ClusterCen[c][d];                                                          
-      Dis_c2m = sqrt( SQR(Vec_c2m[0]) + SQR(Vec_c2m[1]) + SQR(Vec_c2m[2]) );
- 
-//    vectors for calculating the distance between cells and the jet sources
-      for (int d=0; d<3; d++)    TempVec[d] = ClusterCen[c][d] + Jet_Vec[c][d];
- 
-//    distance: temporary vector to mesh
-      for (int d=0; d<3; d++)    Vec_v2m[d] = Pos[d] - TempVec[d];
-      Dis_v2m = sqrt( SQR(Vec_v2m[0]) + SQR(Vec_v2m[1]) + SQR(Vec_v2m[2]) );
- 
-//    distance: jet center to temporary vector
-      Dis_c2v = sqrt( SQR(Jet_Vec[c][0]) + SQR(Jet_Vec[c][1]) + SQR(Jet_Vec[c][2]) );
- 
-//    check whether or not the target cell is within the jet source
-      S      = 0.5*( Dis_c2m + Dis_v2m + Dis_c2v );
-      Area   = sqrt( S*(S-Dis_c2m)*(S-Dis_v2m)*(S-Dis_c2v) );
-      Jet_dr = 2.0*Area/Dis_c2v;
-      Jet_dh = sqrt( Dis_c2m*Dis_c2m - Jet_dr*Jet_dr );
- 
-      if ( Jet_dh <= Jet_HalfHeight[c]*Threshold[lv]  &&  Jet_dr <= Jet_Radius[c]*Threshold[lv]  &&  lv >= 5 ) {
+      double R_SQR = SQR(Pos[0]-ClusterCen[c][0])+SQR(Pos[1]-ClusterCen[c][1])+SQR(Pos[2]-ClusterCen[c][2]);
+      if (  R_SQR <= SQR(R_acc)  &&  R_acc/dh <= Threshold[0]  ){ 
          Flag = true;
-         Aux_Message( stdout, "lv = %d, Threshold = %f\n", lv, Threshold[lv]);
-//         Aux_Message( stdout, " Yes=======================================\n" );
          return Flag;
       }
    }
