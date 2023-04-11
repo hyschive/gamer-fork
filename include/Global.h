e5984af7edf7729f51945f638668752b7688a586
Shuang-Shuang Chen <shuangshuang0411@gmail.com>
2023-04-11 17:00:56 +0800

    Add the ExactCooling SourceTerm (haven't tested) 2

--- include/Global.h
+++ include/Global.h
@@ -271,6 +271,8 @@ extern SrcTerms_t SrcTerms;
 #if ( MODEL == HYDRO )
 extern double     Src_Dlep_AuxArray_Flt[SRC_NAUX_DLEP];
 extern int        Src_Dlep_AuxArray_Int[SRC_NAUX_DLEP];
+extern double     Src_EC_AuxArray_Flt[SRC_NAUX_EC];
+extern int        Src_EC_AuxArray_Int[SRC_NAUX_EC];
 #endif
 extern double     Src_User_AuxArray_Flt[SRC_NAUX_USER];
 extern int        Src_User_AuxArray_Int[SRC_NAUX_USER];
@@ -357,6 +359,12 @@ extern double     (*h_Corner_Array_S[2])[3];
 #if ( MODEL == HYDRO )
 extern real       (*h_SrcDlepProf_Data)[SRC_DLEP_PROF_NBINMAX];
 extern real        *h_SrcDlepProf_Radius;
+#endif
+
+#if ( MODEL == HYDRO )
+extern real        *h_SrcEC_TEF_lambda;
+extern real        *h_SrcEC_TEF_alpha;
+extern real        *h_SrcEC_TEFc;
 #endif
 
 
