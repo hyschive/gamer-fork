f347f8e7547a0cf5ee5b3a27b1c2ba57fa07d0d7
Inhyeok-Song <ihsong@gapp.nthu.edu.tw>
2024-04-10 00:23:26 +0800

    Add minimum & maximum angular resolution criterion for CCSN problems

--- src/TestProblem/Hydro/CCSN/Flag_CCSN.cpp
+++ src/TestProblem/Hydro/CCSN/Flag_CCSN.cpp
@@ -1,14 +1,19 @@
 #include "GAMER.h"
 
 
+static double RAD2DEG = 5.729577951308232e1;
+
+
 extern bool   CCSN_CC_MaxRefine_Flag1;
 extern bool   CCSN_CC_MaxRefine_Flag2;
 extern int    CCSN_CC_MaxRefine_LV1;
 extern int    CCSN_CC_MaxRefine_LV2;
 extern double CCSN_CC_MaxRefine_Dens1;
 extern double CCSN_CC_MaxRefine_Dens2;
-extern double CCSN_MaxRefine_RadFac;
 extern double CCSN_CentralDens;
+
+extern double CCSN_Min_AngRes;
+extern double CCSN_Max_AngRes;
 
 
 
@@ -34,45 +39,43 @@ extern double CCSN_CentralDens;
 bool Flag_CoreCollapse( const int i, const int j, const int k, const int lv, const int PID, const double *Threshold )
 {
 
-   bool Flag = false;
+   bool Flag      = false;
    bool MaxRefine = false;
 
    const double dh        = amr->dh[lv];
-   const double Center[3] = { amr->BoxCenter[0], amr->BoxCenter[1], amr->BoxCenter[2] };
    const double Pos   [3] = { amr->patch[0][lv][PID]->EdgeL[0] + (i+0.5)*dh,
                               amr->patch[0][lv][PID]->EdgeL[1] + (j+0.5)*dh,
                               amr->patch[0][lv][PID]->EdgeL[2] + (k+0.5)*dh  };
-
-   const double dx = Center[0] - Pos[0];
-   const double dy = Center[1] - Pos[1];
-   const double dz = Center[2] - Pos[2];
-   const double r  = sqrt(  SQR( dx ) + SQR( dy ) + SQR( dz )  );
+   const double Center[3] = { 0.5*amr->BoxSize[0], 0.5*amr->BoxSize[1], 0.5*amr->BoxSize[2] };
+   const double dR[3]     = { Pos[0]-Center[0], Pos[1]-Center[1], Pos[2]-Center[2] };
+   const double R         = sqrt( SQR(dR[0]) + SQR(dR[1]) + SQR(dR[2]) );
 
    const double CentralDens = CCSN_CentralDens / UNIT_D;
 
 // (1) check if the allowed maximum level is reached
-   if ( CCSN_CC_MaxRefine_Flag1  &&  CentralDens < CCSN_CC_MaxRefine_Dens1 / UNIT_D )
-   {
+   if      ( CCSN_CC_MaxRefine_Flag1  &&  CentralDens < CCSN_CC_MaxRefine_Dens1 / UNIT_D )
       MaxRefine = lv >= CCSN_CC_MaxRefine_LV1;
-   }
 
    else if ( CCSN_CC_MaxRefine_Flag2  &&  CentralDens < CCSN_CC_MaxRefine_Dens2 / UNIT_D )
-   {
       MaxRefine = lv >= CCSN_CC_MaxRefine_LV2;
-   }
 
 
 // (2) always refined to highest level in the region with r < 30 km
    if ( !MaxRefine )
    {
 //    (2-a) always refine the innermost cells
-      if ( r < amr->dh[lv] )
+      if ( R < amr->dh[lv] )
          Flag = true;
 
 //    (2-b) refine the region with r < 30 km
-      if ( r * UNIT_L < 3e6 )
+      if ( R * UNIT_L < 3e6 )
          Flag = true;
    }
+
+// (3) check if the minimum angular resolution (in degree) is reached
+   if ( CCSN_Min_AngRes > 0.0  && !MaxRefine  &&  !Flag )
+      if ( R * CCSN_Min_AngRes < dh * RAD2DEG )
+         Flag = true;
 
 
    return Flag;
@@ -107,36 +110,81 @@ bool Flag_Lightbulb( const int i, const int j, const int k, const int lv, const 
    bool Flag = false;
 
    const double dh        = amr->dh[lv];
-   const double Center[3] = { amr->BoxCenter[0], amr->BoxCenter[1], amr->BoxCenter[2] };
    const double Pos   [3] = { amr->patch[0][lv][PID]->EdgeL[0] + (i+0.5)*dh,
                               amr->patch[0][lv][PID]->EdgeL[1] + (j+0.5)*dh,
                               amr->patch[0][lv][PID]->EdgeL[2] + (k+0.5)*dh  };
-
-   const double dx = Center[0] - Pos[0];
-   const double dy = Center[1] - Pos[1];
-   const double dz = Center[2] - Pos[2];
-   const double r  = sqrt(  SQR( dx ) + SQR( dy ) + SQR( dz )  );
+   const double Center[3] = { 0.5*amr->BoxSize[0], 0.5*amr->BoxSize[1], 0.5*amr->BoxSize[2] };
+   const double dR[3]     = { Pos[0]-Center[0], Pos[1]-Center[1], Pos[2]-Center[2] };
+   const double R         = sqrt( SQR(dR[0]) + SQR(dR[1]) + SQR(dR[2]) );
 
    const real (*Rho )[PS1][PS1] = amr->patch[ amr->FluSg[lv] ][lv][PID]->fluid[DENS];
 
 
 // TODO: fine-tune the criteria
 // (1) always refined to highest level in the region with r < 30 km
-   if ( r * UNIT_L < 3e6 )
+   if      ( R * UNIT_L < 3e6 )
       Flag = true;
 
-   else
-   {
-//    (2-a) density is larger than the threshold in Input__Flag_User
-      if ( Rho[k][j][i] < Threshold[0] )   return false;
+// (2) density is larger than the threshold in Input__Flag_User
+   else if ( Rho[k][j][i] < Threshold[0] )
+      Flag = false;
 
-//    (2-b) the cell width at son level (lv+1) is larger than the threshold
-      const double Min_CellWidth = r * CCSN_MaxRefine_RadFac;
-
-      Flag = ( 0.5 * dh ) > Min_CellWidth;
-   }
+// (3) check if the minimum angular resolution (in degree) is reached
+   if ( CCSN_Min_AngRes > 0.0  &&  !Flag )
+      if ( R * CCSN_Min_AngRes < dh * RAD2DEG )
+         Flag = true;
 
 
    return Flag;
 
 } // FUNCTION : Flag_Lightbulb
+
+
+
+//-------------------------------------------------------------------------------------------------------
+// Function    :  Flag_Region_CCSN
+// Description :  Check if the element (i,j,k) of the input patch is within the regions allowed to be refined
+//                for the CCSN test problem
+//
+// Note        :  1. Invoked by Flag_Check() using the function pointer "Flag_Region_Ptr",
+//                   which must be set by a test problem initializer
+//                2. Enabled by the runtime option "OPT__FLAG_REGION"
+//
+// Parameter   :  i,j,k       : Indices of the target element in the patch ptr[0][lv][PID]
+//                lv          : Refinement level of the target patch
+//                PID         : ID of the target patch
+//
+// Return      :  "true/false"  if the input cell "is/is not" within the region allowed for refinement
+//-------------------------------------------------------------------------------------------------------
+bool Flag_Region_CCSN( const int i, const int j, const int k, const int lv, const int PID )
+{
+
+   const double dh     = amr->dh[lv];
+   const double Pos[3] = { amr->patch[0][lv][PID]->EdgeL[0] + (i+0.5)*dh,
+                           amr->patch[0][lv][PID]->EdgeL[1] + (j+0.5)*dh,
+                           amr->patch[0][lv][PID]->EdgeL[2] + (k+0.5)*dh  };
+
+   bool Within = true;
+
+
+   const double Center[3] = { 0.5*amr->BoxSize[0], 0.5*amr->BoxSize[1], 0.5*amr->BoxSize[2] };
+   const double dR[3]     = { Pos[0]-Center[0], Pos[1]-Center[1], Pos[2]-Center[2] };
+   const double R         = sqrt( SQR(dR[0]) + SQR(dR[1]) + SQR(dR[2]) );
+
+// check minimum angular resolution (in degree) is reached
+   if ( CCSN_Min_AngRes > 0.0 )
+      if ( R * CCSN_Min_AngRes < dh * RAD2DEG )
+         return true;
+
+// check allowed maximum refine level based on angular resolution (in degree)
+   if ( CCSN_Max_AngRes > 0.0 )
+   {
+      if ( 2.0 * R * CCSN_Max_AngRes > dh * RAD2DEG )   Within = false;
+
+      if ( !Within )   return Within;
+   }
+
+
+   return Within;
+
+} // FUNCTION : Flag_Region_CCSN
--- src/TestProblem/Hydro/CCSN/Init_TestProb_Hydro_CCSN.cpp
+++ src/TestProblem/Hydro/CCSN/Init_TestProb_Hydro_CCSN.cpp
@@ -55,7 +55,6 @@ static int        CCSN_Eint_Mode;                  // Mode of obtaining internal
        double     CCSN_CC_MaxRefine_Dens2;         // central density threshold that reduces the maximum refinement level 2
        double     CCSN_CC_CentralDensFac;          // factor that reduces the dt constrained by the central density (in cgs) during the core collapse
        double     CCSN_CC_Red_DT;                  // reduced time step (in s) when the central density exceeds CCSN_CC_CentralDensFac before bounce
-       double     CCSN_MaxRefine_RadFac;           // factor that determines the maximum refinement level based on distance from the box center
        double     CCSN_LB_TimeFac;                 // factor that scales the dt constrained by lightbulb scheme
        int        CCSN_CC_Rot;                     // mode for rotational profile (0:off, 1:analytical, 2:table)
                                                    // --> analytical formula: Omega(r)=Omega_0*[R_0^2/(r^2+R_0^2)], where r is the spherical radius
@@ -65,6 +64,8 @@ static int        CCSN_Eint_Mode;                  // Mode of obtaining internal
 
        bool       CCSN_Is_PostBounce = false;      // boolean that indicates whether core bounce has occurred
 
+       double     CCSN_Min_AngRes;                 // Minimum angular resolution in degree
+       double     CCSN_Max_AngRes;                 // Maximum angular resolution in degree
        double     CCSN_Shock_ThresFac_Pres;        // pressure threshold factor for detecting postbounce shock
        double     CCSN_Shock_ThresFac_Vel;         // velocity threshold facotr for detecting postbounce shock
        int        CCSN_Shock_Weight;               // weighting of each cell    for detecting postbounce shock (1:volume, 2:1/volume)
@@ -78,6 +79,7 @@ void   Detect_CoreBounce();
 void   Detect_Shock();
 double Mis_GetTimeStep_Lightbulb( const int lv, const double dTime_dt );
 double Mis_GetTimeStep_CoreCollapse( const int lv, const double dTime_dt );
+bool   Flag_Region_CCSN( const int i, const int j, const int k, const int lv, const int PID );
 bool   Flag_CoreCollapse( const int i, const int j, const int k, const int lv, const int PID, const double *Threshold );
 bool   Flag_Lightbulb( const int i, const int j, const int k, const int lv, const int PID, const double *Threshold );
 
@@ -170,13 +172,14 @@ void SetParameter()
    ReadPara->Add( "CCSN_CC_MaxRefine_Dens2",  &CCSN_CC_MaxRefine_Dens2,  1.0e12,        0.0,              NoMax_double      );
    ReadPara->Add( "CCSN_CC_CentralDensFac",   &CCSN_CC_CentralDensFac,   1.0e13,        Eps_double,       NoMax_double      );
    ReadPara->Add( "CCSN_CC_Red_DT",           &CCSN_CC_Red_DT,           1.0e-5,        Eps_double,       NoMax_double      );
-   ReadPara->Add( "CCSN_MaxRefine_RadFac",    &CCSN_MaxRefine_RadFac,    0.15,          0.0,              NoMax_double      );
    ReadPara->Add( "CCSN_LB_TimeFac",          &CCSN_LB_TimeFac,          0.1,           Eps_double,       1.0               );
    ReadPara->Add( "CCSN_CC_Rot",              &CCSN_CC_Rot,              2,             0,                2                 );
    ReadPara->Add( "CCSN_CC_Rot_R0",           &CCSN_CC_Rot_R0,           2.0e8,         Eps_double,       NoMax_double      );
    ReadPara->Add( "CCSN_CC_Rot_Omega0",       &CCSN_CC_Rot_Omega0,       0.5,           0.0,              NoMax_double      );
    ReadPara->Add( "CCSN_CC_Rot_Fac",          &CCSN_CC_Rot_Fac,          -1.0,          NoMin_double,     NoMax_double      );
    ReadPara->Add( "CCSN_Is_PostBounce",       &CCSN_Is_PostBounce,       false,         Useless_bool,     Useless_bool      );
+   ReadPara->Add( "CCSN_Min_AngRes",          &CCSN_Min_AngRes,         -1.0,           NoMin_double,     NoMax_double      );
+   ReadPara->Add( "CCSN_Max_AngRes",          &CCSN_Max_AngRes,         -1.0,           NoMin_double,     NoMax_double      );
    ReadPara->Add( "CCSN_Shock_ThresFac_Pres", &CCSN_Shock_ThresFac_Pres, 0.5,           Eps_double,       NoMax_double      );
    ReadPara->Add( "CCSN_Shock_ThresFac_Vel" , &CCSN_Shock_ThresFac_Vel,  0.1,           Eps_double,       NoMax_double      );
    ReadPara->Add( "CCSN_Shock_Weight" ,       &CCSN_Shock_Weight,        2,             1,                2                 );
@@ -274,6 +277,11 @@ void SetParameter()
          Aux_Error( ERROR_INFO, "Incorrect parameter %s = %d !!\n", "CCSN_Is_PostBounce", CCSN_Is_PostBounce );
    }
 
+// CCSN_Min_AngRes must be larger than CCSN_Min_AngRes
+   if ( CCSN_Min_AngRes > 0.0  &&  CCSN_Max_AngRes > 0.0 )
+   if ( CCSN_Min_AngRes >= CCSN_Max_AngRes )
+      Aux_Error( ERROR_INFO, "%s = %d should be larger than %s = %d  !!\n", "CCSN_Min_AngRes", CCSN_Min_AngRes, "CCSN_Max_AngRes", CCSN_Max_AngRes );
+
 
 // (2) set the problem-specific derived parameters
 
@@ -311,7 +319,6 @@ void SetParameter()
       Aux_Message( stdout, "  sampling interval of GW signals                     = %13.7e\n", CCSN_GW_DT               );
       Aux_Message( stdout, "  mode for obtaining internal energy                  = %d\n",     CCSN_Eint_Mode           );
       if ( CCSN_Prob != Migration_Test ) {
-      Aux_Message( stdout, "  radial factor for maximum refine level              = %13.7e\n", CCSN_MaxRefine_RadFac    );
       Aux_Message( stdout, "  scaling factor for lightbulb dt                     = %13.7e\n", CCSN_LB_TimeFac          );
       Aux_Message( stdout, "  has core bounce occurred                            = %d\n",     CCSN_Is_PostBounce       );
       Aux_Message( stdout, "  pressure threshold factor for detecting shock       = %13.7e\n", CCSN_Shock_ThresFac_Pres );
@@ -332,6 +339,8 @@ void SetParameter()
       Aux_Message( stdout, "  central angular frequency Omega_0 (in rad/s)        = %13.7e\n", CCSN_CC_Rot_Omega0       ); }
       if ( CCSN_CC_Rot == 2 )
       Aux_Message( stdout, "  multiplication factor for rotational profile        = %13.7e\n", CCSN_CC_Rot_Fac          );
+      Aux_Message( stdout, "  minimum angular resolution                          = %13.7e\n", CCSN_Min_AngRes          );
+      Aux_Message( stdout, "  maximum angular resolution                          = %13.7e\n", CCSN_Max_AngRes          );
       Aux_Message( stdout, "=======================================================================================\n"  );
    }
 
@@ -932,6 +941,9 @@ void Init_TestProb_Hydro_CCSN()
 #  if ( EOS == EOS_NUCLEAR  &&  NUC_TABLE_MODE == NUC_TABLE_MODE_TEMP )
    Flu_ResetByUser_Func_Ptr = Flu_ResetByUser_CCSN;
 #  endif
+
+   if ( CCSN_Prob != 0 )
+      Flag_Region_Ptr = Flag_Region_CCSN;
 
 #  ifdef MHD
    switch ( CCSN_Mag )
