d9569f62a4321ccbbd79d516c17cbcc1353e5705
jchan192 <jchan@astr.tohoku.ac.jp>
2024-10-04 11:30:14 +0800

    update IC

--- example/test_problem/ELBDM/LSS_Hybrid_Zoomin/download_IC_lighthalo.sh
+++ example/test_problem/ELBDM/LSS_Hybrid_Zoomin/download_IC_lighthalo.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 API_URL="https://girder.hub.yt/api/v1"
-FILE_ID="66ea79b4999605c485c8d623"
+FILE_ID="66ff5e15999605c485c8d67d"
 LOCAL_FILE="Zoomin_IC"
 
 # download
 girder-cli --api-url ${API_URL} download --parent-type item ${FILE_ID} ${LOCAL_FILE}
 
 # unzip
-tar zxvf ${LOCAL_FILE}/Zoomin_IC_lowres.tar.gz
+tar zxvf ${LOCAL_FILE}/Zoomin_IC_lighthalo.tar.gz
 rm -r ${LOCAL_FILE}
-ln -fs UM_IC_hybrid_N256 UM_IC 
+ln -fs UM_IC_hybrid_N256_third UM_IC 
