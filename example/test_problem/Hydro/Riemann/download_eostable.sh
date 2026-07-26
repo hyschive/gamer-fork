592868a24be0ff6eddd414231bf102252481483f
Hsi-Yu Schive <hyschive@gmail.com>
2026-07-26 16:53:05 +0800

    Add -L to Riemann/download_eostable.sh

--- example/test_problem/Hydro/Riemann/download_eostable.sh
+++ example/test_problem/Hydro/Riemann/download_eostable.sh
@@ -4,4 +4,4 @@ LOCAL_FILENAME="LS220.h5"
 FILE_ID="678716b0999605c485c8ded3"
 
 # 1. download
-curl https://hub.yt/api/v1/item/${FILE_ID}/download -o "${LOCAL_FILENAME}"
+curl -L https://hub.yt/api/v1/item/${FILE_ID}/download -o "${LOCAL_FILENAME}"
