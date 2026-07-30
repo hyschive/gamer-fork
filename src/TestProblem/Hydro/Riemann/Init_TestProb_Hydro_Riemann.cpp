a9c1babeb1414e81bcb82035e1c6d5fcc433b256
Hsi-Yu Schive <hyschive@gmail.com>
2026-07-30 21:34:55 +0800

    Correct the upper bound of Riemann_Prob

--- src/TestProblem/Hydro/Riemann/Init_TestProb_Hydro_Riemann.cpp
+++ src/TestProblem/Hydro/Riemann/Init_TestProb_Hydro_Riemann.cpp
@@ -139,7 +139,7 @@ void LoadInputTestProb( const LoadParaMode_t load_mode, ReadPara_t *ReadPara, HD
 // ************************************************************************************************************************
 // LOAD_PARA( load_mode, "KEY_IN_THE_FILE",   &VARIABLE,               DEFAULT,      MIN,              MAX               );
 // ************************************************************************************************************************
-   LOAD_PARA( load_mode, "Riemann_Prob",      &Riemann_Prob,          -1,            0,                12                );
+   LOAD_PARA( load_mode, "Riemann_Prob",      &Riemann_Prob,          -1,            0,                14                );
    LOAD_PARA( load_mode, "Riemann_LR",        &Riemann_LR,             1,            NoMin_int,        NoMax_int         );
    LOAD_PARA( load_mode, "Riemann_XYZ",       &Riemann_XYZ,            0,            0,                2                 );
    LOAD_PARA( load_mode, "Riemann_RhoL",      &Riemann_RhoL,           __DBL_MAX__,  __DBL_MIN__,      __DBL_MAX__       );
